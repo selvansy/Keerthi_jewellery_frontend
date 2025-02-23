d737a28e8d865209c37602950b03d69201d42c34
PunniyamoorthyAtts <punniyamoorthy.b@atts.in>
2025-02-23 18:18:47 +0530

    Update spinLoading.jsx

--- src/chit/components/common/spinLoading.jsx
+++ src/chit/components/common/spinLoading.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { RotatingLines } from "react-loader-spinner";
  
-function SpinLoading({color='white'}) {
+function SpinLoading() {
   return (
     <div className="flex justify-center">
       <RotatingLines
         visible={true}
         height="10"
         width="26"
-        strokeColor={color}
+        strokeColor="white"
         strokeWidth="5"
         animationDuration="0.75"
         ariaLabel="rotating-lines-loading"
