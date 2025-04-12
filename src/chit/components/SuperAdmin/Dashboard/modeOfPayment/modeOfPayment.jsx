6788e91967339211f7254c0c178b56ed28ca9254
NeeharpremAtts <neeharprem@atts.in>
2025-04-12 09:47:20 +0530

    fixed button

--- src/chit/components/SuperAdmin/Dashboard/modeOfPayment/modeOfPayment.jsx
+++ src/chit/components/SuperAdmin/Dashboard/modeOfPayment/modeOfPayment.jsx
@@ -94,8 +94,8 @@ function ModeOfPayment() {
         />
       </div>
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 my-5">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500  my-5">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50  ">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Payment Mode
@@ -106,49 +106,49 @@ function ModeOfPayment() {
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr className="bg-white border-b  border-gray-200">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Bank Transfer
               </th>
               <td className="px-6 py-4">1500</td>
             </tr>
 
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr className="bg-white border-b  border-gray-200">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Gpay
               </th>
               <td className="px-6 py-4">1000</td>
             </tr>
 
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr className="bg-white border-b  border-gray-200">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Credit Card
               </th>
               <td className="px-6 py-4">1000</td>
             </tr>
 
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr className="bg-white border-b  border-gray-200">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Debit Card
               </th>
               <td className="px-6 py-4">5000</td>
             </tr>
-            <tr className="bg-white  dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+            <tr className="bg-white   border-gray-200">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
               >
                 Razor Pay
               </th>
